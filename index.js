4a1741b0b1479d4e7b73f92237d9feb20a57c7e3
Veronica HW <veronica.wajda@gmail.com>
2020-04-20 17:54:38 -0600

    added joi

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 
+const cideries = [
+  { id: 1, name: 'cider1'},
+  { id: 2, name: 'cider2'},
+  { id: 3, name: 'cider3'}
+]
+
 app.get('/api/cideries', (req, res) => {
-  res.send('Hello World');
+  res.send(cideries);
 });
 
 app.get('/api/cideries/:id', (req, res) => {
-  req.params.id;
+  const cidery = cideries.find(cidery => cidery.id === parseInt(req.params.id));
+  if(!cidery) return res.status(404).send('not found');
+  res.send(cidery);
 });
 
 const port = process.env.PORT || 3000;
