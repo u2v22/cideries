6d8e1d77615194350caddcb6dd49221895b60b61
Veronica HW <veronica.wajda@gmail.com>
2020-04-20 18:45:54 -0600

    added validation on put request too

--- index.js
+++ index.js
@@ -36,6 +36,21 @@ app.post('/api/cideries', (req, res) => {
   res.send(cidery);
 });
 
+app.put('/api/cideries/:id', (req, res) => {
+  const cidery = cideries.find(cidery => cidery.id === parseInt(req.params.id));
+  if(!cidery) return res.status(404).send('not found');
+
+  const { error } = validationCheck(req.body);
+  if(error) return res.status(400).send(error.details[0]);
+
+  cidery.name = req.body.name;
+  res.send(cidery);
+});
+
+app.delete('/api/cideries/:id', (req, res) => {
+
+});
+
 
 const validationCheck = (args) => {
   const schema = Joi.object().keys({
